4b5e5536cb5ace0d4546866b7d60d51cb4d54b7f
nataliam <nataliam@student.21-school.ru>
2024-01-29 18:07:03 +0300

    add test is_equal and edit s21_decimal.h

--- s21_decimal_lib/s21_decimal.h
+++ s21_decimal_lib/s21_decimal.h
@@ -1,6 +1,19 @@
 #ifndef MEGA_HOROSH_DECIMAL_S21_DECIMAL_H
 #define MEGA_HOROSH_DECIMAL_S21_DECIMAL_H
 
+#include <stdio.h>
+
+typedef struct {
+  unsigned int bits[4];
+} s21_decimal;
+
 int sum(int a, int b);
 
-#endif //MEGA_HOROSH_DECIMAL_S21_DECIMAL_H
+int s21_is_less(s21_decimal a, s21_decimal b);
+int s21_is_less_or_equal(s21_decimal a, s21_decimal b);
+int s21_is_greater(s21_decimal a, s21_decimal b);
+int s21_is_greater_or_equal(s21_decimal a, s21_decimal b);
+int s21_is_equal(s21_decimal a, s21_decimal b);
+int s21_is_not_equal(s21_decimal a, s21_decimal b);
+
+#endif  // MEGA_HOROSH_DECIMAL_S21_DECIMAL_H

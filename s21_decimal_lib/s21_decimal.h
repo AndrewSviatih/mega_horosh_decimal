84a28b8dfac8b59c9aa95f05652566f605c8c81f
Kara Jude <karajude@qu-g7.nov.21-school.ru>
2024-01-30 10:16:45 +0300

    test decimal

--- s21_decimal_lib/s21_decimal.h
+++ s21_decimal_lib/s21_decimal.h
@@ -4,4 +4,11 @@
 #include "stdio.h"
 #include "stdlib.h"
 
+#define MINUS 0x80000000
+#define SC 0x00ff0000
+
+typedef struct {
+  int bits[4];
+} s21_decimal;
+
 #endif //MEGA_HOROSH_DECIMAL_S21_DECIMAL_H
--- s21_decimal_lib/test_main.c
+++ s21_decimal_lib/test_main.c
@@ -1,28 +1,41 @@
 
 #include "s21_decimal.h"
+
 void printBinary(int num) {
     int i;
     for (i = sizeof(int) * 8 - 1; i >= 0; --i) {
         printf("%d", (num >> i) & 1);
+
+        if (i % 8 == 0) {
+            printf(" ");
+        }
     }
     printf("\n");
 }
 
 int main () {
 
-  unsigned char var = 11;
-  unsigned char not_var = 10;
+  int var = -9;
+  int not_var = 1;
 
-  unsigned char res = var & not_var;
-
-  if ((var & not_var) == not_var) {
-    printf("bit is on\n");
+  if (var & MINUS) {
+    printf("minus\n");
   } else {
-    printf("no\n");
+    printf("plus\n");
+  }
+
+  unsigned char res = var | MINUS;
+
+  if (not_var & MINUS) {
+    printf("minus\n");
+  } else {
+    printf("plus\n");
   }
 
   printf("%d %d %d\n", var, not_var, res);
   printBinary(var);
   printBinary(not_var);
   printBinary(res);
+
+  return 0;
 }

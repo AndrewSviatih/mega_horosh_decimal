f92fbf474650295238cf6efcc84f524e534bc814
nataliam <nataliam@student.21-school.ru>
2024-02-01 00:09:34 +0300

    main.c

--- main.c
+++ main.c
@@ -1,32 +1,30 @@
 #include "tests/s21_tests.h"
 
 int main(void) {
-    run_tests();
+  run_tests();
 
-    return 0;
+  return 0;
 }
 
 void run_testcase(Suite *testcase, int counter_testcase) {
-
-    if (counter_testcase > 1)
-        putchar('\n');
-    printf("%s%d%s", "CURRENT TEST: ", counter_testcase, "\n");
-    counter_testcase++;
-    SRunner *sr = srunner_create(testcase);
-    srunner_set_fork_status(sr, CK_NOFORK);
-    srunner_run_all(sr, CK_NORMAL);
-    srunner_free(sr);
+  if (counter_testcase > 1) putchar('\n');
+  printf("%s%d%s", "CURRENT TEST: ", counter_testcase, "\n");
+  counter_testcase++;
+  SRunner *sr = srunner_create(testcase);
+  srunner_set_fork_status(sr, CK_NOFORK);
+  srunner_run_all(sr, CK_NORMAL);
+  srunner_free(sr);
 }
 
 void run_tests(void) {
+  int counter_testcase = 0;
 
-    int counter_testcase = 0;
+  Suite *list_cases[] = {sample(), suite_is_equal(), suite_is_not_equal(),
+                         NULL};
 
-    Suite *list_cases[] = {sample(), NULL};
-
-    for (Suite **current_testcase = list_cases; *current_testcase != NULL;
-        current_testcase++) {
-        run_testcase(*current_testcase, counter_testcase);
-        counter_testcase++;
-    }
+  for (Suite **current_testcase = list_cases; *current_testcase != NULL;
+       current_testcase++) {
+    run_testcase(*current_testcase, counter_testcase);
+    counter_testcase++;
+  }
 }

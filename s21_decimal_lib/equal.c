e115e65b4acaef41f100c9a8cced3763b1132e6c
laraeter <git28282@gmail.com>
2024-02-09 16:05:40 +0300

    finish

--- s21_decimal_lib/equal.c
+++ s21_decimal_lib/equal.c
@@ -1,13 +1,126 @@
-#include <stdbool.h>
-
 #include "s21_decimal.h"
+
+int s21_is_less(s21_decimal value_1, s21_decimal value_2) {
+  bool sign_num_1 = 0, sign_num_2 = 0, sign = 0;
+  if (value_1.bits[3] & MINUS) sign_num_1 = 1;
+  if (value_2.bits[3] & MINUS) sign_num_2 = 1;
+  int res = 0;
+  work_decimal work_value_1 = conversion(value_1);
+  work_decimal work_value_2 = conversion(value_2);
+  while (work_value_1.scale > work_value_2.scale) pointleft(&work_value_2);
+  while (work_value_2.scale > work_value_1.scale) pointleft(&work_value_1);
+
+  if (less(work_value_1, work_value_2)) res = 1;
+
+  if (sign_num_1 && !sign_num_2) {
+    res = 1;
+  } else if (sign_num_1 && sign_num_2 && notEqual(work_value_1, work_value_2)) {
+    res = !res;
+  } else if (!sign_num_1 && sign_num_2) {
+    res = 0;
+  }
+  return res;
+}
+
+int s21_is_less_or_equal(s21_decimal value_1, s21_decimal value_2) {
+  bool sign_num_1 = 0, sign_num_2 = 0, sign = 0;
+  if (value_1.bits[3] & MINUS) sign_num_1 = 1;
+  if (value_2.bits[3] & MINUS) sign_num_2 = 1;
+  int res = 0;
+  work_decimal work_value_1 = conversion(value_1);
+  work_decimal work_value_2 = conversion(value_2);
+  while (work_value_1.scale > work_value_2.scale) pointleft(&work_value_2);
+  while (work_value_2.scale > work_value_1.scale) pointleft(&work_value_1);
+  if (lessOrEqual(work_value_1, work_value_2)) res = 1;
+
+  if (sign_num_1 && !sign_num_2) {
+    res = 1;
+  } else if (sign_num_1 && sign_num_2 && notEqual(work_value_1, work_value_2)) {
+    res = !res;
+  } else if (!sign_num_1 && sign_num_2) {
+    res = 0;
+  }
+  return res;
+}
+
+int s21_is_greater(s21_decimal value_1, s21_decimal value_2) {
+  bool sign_num_1 = 0, sign_num_2 = 0, sign = 0;
+  if (value_1.bits[3] & MINUS) sign_num_1 = 1;
+  if (value_2.bits[3] & MINUS) sign_num_2 = 1;
+  int res = 0;
+  work_decimal work_value_1 = conversion(value_1);
+  work_decimal work_value_2 = conversion(value_2);
+  while (work_value_1.scale > work_value_2.scale) pointleft(&work_value_2);
+  while (work_value_2.scale > work_value_1.scale) pointleft(&work_value_1);
+  if (more(work_value_1, work_value_2)) res = 1;
+
+  if (sign_num_1 && !sign_num_2) {
+    res = 0;
+  } else if (sign_num_1 && sign_num_2 && notEqual(work_value_1, work_value_2)) {
+    res = !res;
+  } else if (!sign_num_1 && sign_num_2) {
+    res = 1;
+  }
+  return res;
+}
+
+int s21_is_greater_or_equal(s21_decimal value_1, s21_decimal value_2) {
+  bool sign_num_1 = 0, sign_num_2 = 0, sign = 0;
+  if (value_1.bits[3] & MINUS) sign_num_1 = 1;
+  if (value_2.bits[3] & MINUS) sign_num_2 = 1;
+  int res = 0;
+  work_decimal work_value_1 = conversion(value_1);
+  work_decimal work_value_2 = conversion(value_2);
+  while (work_value_1.scale > work_value_2.scale) pointleft(&work_value_2);
+  while (work_value_2.scale > work_value_1.scale) pointleft(&work_value_1);
+  if (moreOrEqual(work_value_1, work_value_2)) res = 1;
+
+  if (sign_num_1 && !sign_num_2) {
+    res = 0;
+  } else if (sign_num_1 && sign_num_2 && notEqual(work_value_1, work_value_2)) {
+    res = !res;
+  } else if (!sign_num_1 && sign_num_2) {
+    res = 1;
+  }
+  return res;
+}
+
+int s21_is_equal(s21_decimal value_1, s21_decimal value_2) {
+  bool sign_num_1 = 0, sign_num_2 = 0, sign = 0;
+  if (value_1.bits[3] & MINUS) sign_num_1 = 1;
+  if (value_2.bits[3] & MINUS) sign_num_2 = 1;
+  int res = 0;
+  work_decimal work_value_1 = conversion(value_1);
+  work_decimal work_value_2 = conversion(value_2);
+  while (work_value_1.scale > work_value_2.scale) pointleft(&work_value_2);
+  while (work_value_2.scale > work_value_1.scale) pointleft(&work_value_1);
+  if (equally(work_value_1, work_value_2)) res = 1;
+
+  if (sign_num_1 != sign_num_2) {
+    res = 0;
+  }
+  return res;
+}
+
+int s21_is_not_equal(s21_decimal value_1, s21_decimal value_2) {
+  bool sign_num_1 = 0, sign_num_2 = 0, sign = 0;
+  if (value_1.bits[3] & MINUS) sign_num_1 = 1;
+  if (value_2.bits[3] & MINUS) sign_num_2 = 1;
+  int res = 1;
+  work_decimal work_value_1 = conversion(value_1);
+  work_decimal work_value_2 = conversion(value_2);
+  while (work_value_1.scale > work_value_2.scale) pointleft(&work_value_2);
+  while (work_value_2.scale > work_value_1.scale) pointleft(&work_value_1);
+  if (equally(work_value_1, work_value_2) && sign_num_1 == sign_num_2) res = 0;
+  return res;
+}
 
 bool equally(work_decimal num_1, work_decimal num_2) {
   bool res = true;
   while (num_1.scale > num_2.scale) pointleft(&num_2);
   while (num_2.scale > num_1.scale) pointleft(&num_1);
   for (int i = 6; i >= 0 && res; i--) {
-    if (num_1.bits[i] != num_2.bits[2]) {
+    if (num_1.bits[i] != num_2.bits[i]) {
       res = false;
     }
   }
--- s21_decimal_lib/s21_decimal.h
+++ s21_decimal_lib/s21_decimal.h
@@ -3,6 +3,7 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #define MINUS 0x80000000     // 10000000 00000000 00000000 00000000
 #define SC 0x00ff0000        // 00000000 11111111 00000000 00000000
@@ -37,21 +38,70 @@ bool getoverflow(work_decimal* num);
 void pointleft(work_decimal* num);
 // Сдвиг запятов в право
 uint64_t pointright(work_decimal* num);
+// Проверяет можно ли округлить(удалить лишние нули)
+bool isRoundPosebal(work_decimal num);
 // Нормализация рабочего decimal
 bool normalization(work_decimal* num);
 // Неполное вычитание
 void incompleteSubtraction(work_decimal work_num_1, work_decimal work_num_2,
                            work_decimal* work_res);
 // Сложение
-bool addition(s21_decimal num_1, s21_decimal num_2, s21_decimal* res);
+int s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal* result);
 // Вычитание
-bool subtraction(s21_decimal num_1, s21_decimal num_2, s21_decimal* res);
+int s21_sub(s21_decimal value_1, s21_decimal value_2, s21_decimal* result);
+// Смотрит не вышла ли scale за границы
+bool s21_checkScale(work_decimal* value);
+// Умножение
+int s21_mul(s21_decimal value_1, s21_decimal value_2, s21_decimal* result);
+// умножаем на 10 если можно
+bool mulBy10(work_decimal* work_temp, work_decimal work_value,
+             work_decimal* work_iter);
+// сумма work_decimal
+int addWork(work_decimal work_num_1, work_decimal work_num_2,
+            work_decimal* work_res);
+// вычитание work_decimal
+int subWork(work_decimal work_num_1, work_decimal work_num_2,
+            work_decimal* work_res);
+// Приравнивает к 0 work_decimal
+void setToZero(work_decimal* value);
+// Добавляет 1
+int add1Work(work_decimal* work_res);
+// Деление с остатком. Возвращает остаток
+work_decimal divisionWithRemainder(work_decimal work_value_1,
+                                   work_decimal work_value_2,
+                                   work_decimal* work_result);
+// равно ли число work_decimal нулю
+bool equalToZeroWork(work_decimal work_num);
+// getoverflow где цикл до 3
+bool getoverflow3(work_decimal* num);
+// Можно ли далить
+bool checkDiv(work_decimal work_result, bool* stop);
+// Деление
+int s21_div(s21_decimal value_1, s21_decimal value_2, s21_decimal* result);
 
+// <
+int s21_is_less(s21_decimal value_1, s21_decimal value_2);
+// <=
+int s21_is_less_or_equal(s21_decimal value_1, s21_decimal value_2);
+// >
+int s21_is_greater(s21_decimal value_1, s21_decimal value_2);
+// >=
+int s21_is_greater_or_equal(s21_decimal value_1, s21_decimal value_2);
+// ==
+int s21_is_equal(s21_decimal value_1, s21_decimal value_2);
+// !=
+int s21_is_not_equal(s21_decimal value_1, s21_decimal value_2);
+// work_decimal: ==
 bool equally(work_decimal num_1, work_decimal num_2);
+// work_decimal: !=
 bool notEqual(work_decimal num_1, work_decimal num_2);
+// work_decimal: >
 bool more(work_decimal num_1, work_decimal num_2);
+// work_decimal: <
 bool less(work_decimal num_1, work_decimal num_2);
+// work_decimal: >=
 bool moreOrEqual(work_decimal num_1, work_decimal num_2);
+// work_decimal: <=
 bool lessOrEqual(work_decimal num_1, work_decimal num_2);
 
-#endif // MEGA_HOROSH_DECIMAL_S21_DECIMAL_H
+#endif  // MEGA_HOROSH_DECIMAL_S21_DECIMAL_H

40cf7f2e5ef5ff5fcd3a85925c1e43e51d8836fe
Kara Jude <karajude@le-e4.nov.21-school.ru>
2024-02-12 19:24:10 +0300

    final fix converters

--- s21_decimal_lib/bit_operations.c
+++ s21_decimal_lib/bit_operations.c
@@ -1,7 +1,7 @@
 #include "s21_decimal.h"
 
 int s21_get_bit(s21_decimal dst, int index) {
-  int mask = 1u << ((index) % 32);  // BIT_MASK s21_decimal.h
+  int mask = BIT_MASK(index);  // BIT_MASK s21_decimal.h
 
   return (dst.bits[index / 32] & mask) != 0;
 }  // sample: bit index = 40. 40 / 32 = 1. mask = bit from curr index.
